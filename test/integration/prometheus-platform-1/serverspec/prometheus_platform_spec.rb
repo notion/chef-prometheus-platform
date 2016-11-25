b3dcc1c4c78537afdf2b167f38fd255cf1e3bdcb
Florian Philippon <florian.philippon@s4m.io>
2016-11-25 12:18:34 +0100

    Add kitchen test on prometheus API

--- test/integration/prometheus-platform-1/serverspec/prometheus_platform_spec.rb
+++ test/integration/prometheus-platform-1/serverspec/prometheus_platform_spec.rb
@@ -16,3 +16,29 @@
 
 require 'master'
 require 'grafana'
+
+# Waiting for nodes to be up through Prometheus API
+(1..10).each do |try|
+  curl = 'http_proxy="" curl -s'
+  url = 'http://localhost:9090/api/v1/query?query=up'
+  up_nodes = `#{curl} "#{url}"`
+  break if
+    up_nodes.include?(
+      'prometheus-platform-kitchen-1'
+    ) && up_nodes.include?(
+      'prometheus-platform-kitchen-2'
+    )
+  puts "Rest waiting to Schema Registry to be ready… (##{try}/5)"
+  sleep(5)
+end
+
+describe 'With Prometheus API' do
+  curl = 'http_proxy="" curl -s'
+  url = 'http://localhost:9090/api/v1/query?query=up'
+
+  it 'We can get the list of up nodes' do
+    up_nodes = `#{curl} "#{url}"`
+    expect(up_nodes).to include('prometheus-platform-kitchen-1')
+    expect(up_nodes).to include('prometheus-platform-kitchen-2')
+  end
+end
