a10ba91bafd23ef96a61b3c43ab3e8b72b73148d
Samuel Bernard <sb@make.org>
2018-04-03 16:30:49 +0200

    chore: start 2.1.0

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description 'Cookbook used to install and configure prometheus'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 source_url 'https://gitlab.com/chef-platform/prometheus-platform'
 issues_url 'https://gitlab.com/chef-platform/prometheus-platform/issues'
-version '2.0.0'
+version '2.1.0'
 
 chef_version '>= 12.14'
 
