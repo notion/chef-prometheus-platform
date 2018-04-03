7866a773a7d615a873821656a63a8aeb1de6b4cb
Samuel Bernard <sb@make.org>
2018-04-03 15:24:47 +0200

    chore: change current version to 2.0.0

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description 'Cookbook used to install and configure prometheus'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 source_url 'https://gitlab.com/chef-platform/prometheus-platform'
 issues_url 'https://gitlab.com/chef-platform/prometheus-platform/issues'
-version '1.2.0'
+version '2.0.0'
 
 chef_version '>= 12.14'
 
